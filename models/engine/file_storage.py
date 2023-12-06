aa00baeff110b75f40efb77596fd312fa4568fcb
SalahMSwefy <salahm.swefy@gmali.com>
2023-12-06 21:01:39 +0200

    # AirBnB Website

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+from models.city import City
+
+
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        return self.__objects
+    
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Saves storage dictionary to file"""
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, mode="w") as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        try:
+            with open(self.__file_path, mode="r") as f:
+                new_dict = json.load(f)
+            for key, value in new_dict.items():
+                self.__objects[key] = eval(value["__class__"])(**value)
+        except FileNotFoundError:
+            pass
